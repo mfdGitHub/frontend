51522022ce83dac0455b416735ce72072dc1d286
MoisesDebian11 <moyinformatico@gmail.com>
2022-11-26 17:37:34 -0600

    Se agrego un archivo script y se modifico el index

---
+++ script.js
@@ -0,0 +1,33 @@
+function search(){
+    var resultado = [
+        {
+          "id": 5,
+          "address": "Av. San roque 312",
+          "email": "victoriaperez@yahoo.com",
+          "firstname": "Ray",
+          "lastname": "Perez",
+          "phone": "90909090909"
+        },
+        {
+          "id": 9,
+          "address": "Av. San roque 312",
+          "email": "victoriaperez@yahoo.com",
+          "firstname": "Lucas",
+          "lastname": "Moy",
+          "phone": "90909090909"
+        }
+    ]
+
+    var row = `
+                <tr>
+                    <td>Pepe</td>
+                    <td>Angelini</td>
+                    <td>pep.angelini@gmail.com</td>
+                    <td>4534534</td>
+                    <td>
+                        <a href="#" class="myButton">Editar</a>
+                        <a href="#" class="btnDelete">Elimiar</a>
+                    </td>
+                </tr>    
+            `
+}
